ef31c009be2d0cd9f84ae9ea8218b6aeb0d8a108
Yuuki Wesp <ls-micro@ya.ru>
2019-09-22 05:35:52 +0300

    add parallax mapping and divider rgb color

---
+++ src/Elementary.Shader.Nodes/DivideRGB.cs
@@ -0,0 +1,30 @@
+﻿namespace Elementary.Shader.Nodes
+{
+    using UnityEditor.ShaderGraph;
+    using System.Reflection;
+    [Title("Math")]
+    public class DivideRGB : CodeFunctionNode
+    {
+        public DivideRGB() => name = "Divide RGB";
+
+        protected override MethodInfo GetFunctionToConvert() =>
+            GetType().GetMethod("_Function",
+                BindingFlags.Static | BindingFlags.NonPublic);
+
+
+        static string _Function(
+            [Slot(0, Binding.None)] Vector1 R,
+            [Slot(1, Binding.None)] Vector1 G,
+            [Slot(2, Binding.None)] Vector1 B,
+            [Slot(3, Binding.None)] Vector1 Divider,
+            [Slot(4, Binding.None)] out DynamicDimensionVector Out
+        )
+        {
+            return @"
+{
+	Out = ((R + G + B) / Divider);
+}
+";
+        }
+    }
+}
---
+++ src/Elementary.Shader.Nodes/ParallaxMapping.cs
@@ -0,0 +1,32 @@
+﻿namespace Elementary.Shader.Nodes
+{
+    using System.Reflection;
+    using UnityEditor.ShaderGraph;
+    using UnityEngine;
+
+    [Title("Math", "Advanced")]
+    public class ParallaxMapping : CodeFunctionNode
+    {
+        public ParallaxMapping() => name = "Parallax Mapping";
+
+        protected override MethodInfo GetFunctionToConvert() =>
+            GetType().GetMethod("_Function",
+                BindingFlags.Static | BindingFlags.NonPublic);
+
+
+        static string _Function(
+            [Slot(0, Binding.None)] Vector2 texCoords,
+            [Slot(1, Binding.None)] Vector3 viewDir,
+            [Slot(2, Binding.None)] out DynamicDimensionVector Out
+        )
+        {
+            return @"
+{
+    float height = texture(depthMap, texCoords).r;
+    vec2 p = viewDir.xy / viewDir.z * (height * height_scale);
+	Out = texCoords - p;
+}
+";
+        }
+    }
+}
